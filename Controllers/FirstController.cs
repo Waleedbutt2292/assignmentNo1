b02efdd81f1d10a6b65895ef36d685252e90bc7a
Waleed Butt <buttw935@gmail.com>
2019-09-07 23:12:49 +0500

    Add project files.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewTask.Models;
+
+namespace NewTask.Controllers
+{
+    public class FirstController : Controller
+    {
+        private TableContext T = null;
+        public FirstController(TableContext _T)
+        {
+            T = _T;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddNewCategory()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult AddNewCategory(Categories C)
+
+        {
+            T.Categories.Add(C);
+            T.SaveChanges();
+            return View();
+        }
+
+       public IActionResult ALLCategory()
+        {
+            var O=T.Categories.ToList();
+            return View(O);
+        }
+
+        
+    }
+}
---
+++ Controllers/SecondController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewTask.Models;
+
+namespace NewTask.Controllers
+{
+    public class SecondController : Controller
+    {
+        private TableContext k = null;
+        public SecondController (TableContext _post)
+        {
+            k = _post;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddPost()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPost(Post p)
+        {
+            k.Post.Add(p);
+            k.SaveChanges();
+            return View();
+        }
+        public IActionResult ViewList()
+        {
+            var f=k.Post.ToList();
+            return View(f);
+        }
+    }
+}
